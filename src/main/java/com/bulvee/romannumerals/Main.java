4970058669744fee4c75037af0a9bfad72f1a07a
Alexandre Santos <alexandrehsantos@gmail.com>
2023-02-02 12:37:02 -0300

    changed package default

--- src/main/java/com/bulvee/romannumerals/Main.java
+++ src/main/java/com/bulvee/romannumerals/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.bulvee.romannumerals;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/com/bulvee/romannumerals/RomanTranslator.java
+++ src/main/java/com/bulvee/romannumerals/RomanTranslator.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.bulvee.romannumerals;
 
 import java.util.Arrays;
 import java.util.HashMap;
