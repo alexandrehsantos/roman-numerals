717ca21f737fef28a95036e26f5c2968e23b0cf1
Alexandre Santos <alexandrehsantos@gmail.com>
2023-02-02 12:31:50 -0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        RomanTranslator romanTranslator = new RomanTranslator();
+//        String bigTest = "MMCCM"; --> falta criar validação deste cenário
+        String bigTest = "MMCCM"; // bug
+        int decimalNumber = romanTranslator.romanToInt(bigTest);
+        System.out.println(decimalNumber);
+    }
+}
+//999
